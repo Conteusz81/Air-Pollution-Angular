6aa7792ff32fbbe090924817d7d74bc090de29cc
Piotrek <piotr.pawlica@interia.pl>
2019-07-26 08:28:58 +0200

    dashboard-component name change

---
+++ src/app/dashboard-main-page/dashboard-main-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-page-navigation',
+  templateUrl: './dashboard-main-page-.component.html',
+  styleUrls: ['./dashboard-main-page-.component.scss']
+})
+export class DashboardMainPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
