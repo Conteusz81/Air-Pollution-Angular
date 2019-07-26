1b4c4aa6317b213124efbe38aef6aff9f1ae89c6
Piotrek <piotr.pawlica@interia.pl>
2019-07-26 08:28:18 +0200

    city-search bug fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {CityDetailsComponent} from './city-details/city-details.component';
-import {MainPageNavigationComponent} from './main-page-navigation/main-page-navigation.component';
+import {DashboardMainPageComponent} from './dashboard-main-page/dashboard-main-page.component';
 import {CitySearchComponent} from './city-search/city-search.component';
 import {ParametersListComponent} from './parameters-list/parameters-list.component';
 import {MostPollutedCitiesComponent} from './most-polluted-cities/most-polluted-cities.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: MainPageNavigationComponent },
+  { path: 'home', component: DashboardMainPageComponent },
   { path: 'city', component: CitySearchComponent,
     children: [ { path: ':id', component: CityDetailsComponent } ]},
   { path: 'parameter', component: ParametersListComponent,
