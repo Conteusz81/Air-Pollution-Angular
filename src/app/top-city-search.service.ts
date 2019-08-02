aa64ab52404c4abef02e2fb1241b6bd4ea01f7cf
Piotrek <piotr.pawlica@interia.pl>
2019-08-02 15:26:24 +0200

    Managing Top City Search Service with local storage

--- src/app/top-city-search.service.ts
+++ src/app/top-city-search.service.ts
@@ -1,14 +1,33 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {CityCounter} from './model/city-counter.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TopCitySearchService {
-  test = [];
+  private cityCounter: CityCounter[] = JSON.parse(localStorage.getItem('cityCounter'));
+
   constructor() { }
 
-  getName(x) {
-    this.test.push(x);
-    console.log(this.test);
+  cityChoice(cityName: string) {
+    this.checkIfStorageIsEmpty();
+    const index = this.cityCounter.findIndex( element => element.name === cityName );
+    if ( index === -1) {
+      this.cityCounter.unshift({ name: cityName, counter: 1});
+    } else {
+      this.cityCounter.filter(element => {
+        if (element.name === cityName) {
+          element.counter++;
+        }
+      });
+    }
+    this.cityCounter.sort((a, b) => (a.counter < b.counter) ? 1 : ((b.counter < a.counter) ? -1 : 0));
+    localStorage.setItem('cityCounter' , JSON.stringify(this.cityCounter));
+  }
+
+  private checkIfStorageIsEmpty() {
+    if (this.cityCounter === null) {
+      this.cityCounter = [];
+    }
   }
 }
