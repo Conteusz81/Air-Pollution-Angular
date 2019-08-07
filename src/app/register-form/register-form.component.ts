dcc8a2656a7549a959fe7304741ab61870b5590d
Piotrek <piotr.pawlica@interia.pl>
2019-08-07 14:41:00 +0200

    Register Component password match validation added

--- src/app/register-form/register-form.component.ts
+++ src/app/register-form/register-form.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {AbstractControl, FormBuilder, FormControl, FormGroup, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material';
 
-export const checkPasswordMatch = (control: AbstractControl): {[key: string]: boolean} => {
-  const primaryPassword = control.get('password').value;
-  const secondaryPassword = control.get('confirmPassword').value;
-  if (!primaryPassword || !secondaryPassword) {
-    return null;
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const invalidParent = !!(control && control.parent && control.parent.invalid && control.parent.dirty
+      && control.parent.hasError('notSame'));
+    return (invalidParent);
   }
-  return primaryPassword === secondaryPassword ? null : { notSame: true };
-};
+}
 
 @Component({
   selector: 'app-register-form',
@@ -18,20 +18,30 @@ export const checkPasswordMatch = (control: AbstractControl): {[key: string]: bo
 export class RegisterFormComponent implements OnInit {
 
   private registerForm: FormGroup;
+  private matcher = new MyErrorStateMatcher();
   private hidePrimary = true;
   private hideSecondary = true;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
     this.registerForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       userName: ['', [Validators.required, Validators.minLength(4)]],
-      password: ['', [Validators.required, Validators.minLength(8), Validators.pattern('.*[a-z].*'),
-        Validators.pattern('.*[A-Z].*'), Validators.pattern('.*[0-9].*')]],
-      confirmPassword: ['']
-    }, {validator: checkPasswordMatch});
+      password: ['',
+       [Validators.required,
+        Validators.minLength(8),
+        Validators.pattern('.*[a-z].*'),
+        Validators.pattern('.*[A-Z].*'),
+        Validators.pattern('.*[0-9].*')]],
+      confirmPassword: ['', Validators.required]
+    }, {validator: this.checkPasswordMatch});
   }
 
-  ngOnInit() {
+   checkPasswordMatch(control: AbstractControl): {[key: string]: boolean} {
+    const primaryPassword = control.get('password').value;
+    const secondaryPassword = control.get('confirmPassword').value;
+    return primaryPassword === secondaryPassword ? null : { notSame: true };
   }
 
   getEmailErrorMessage() {
@@ -49,5 +59,18 @@ export class RegisterFormComponent implements OnInit {
       this.registerForm.get('password').hasError('minlength') ? 'Password must have at least 8 characters' :
         this.registerForm.get('password').hasError('pattern') ?
           'Password must have at least one small letter, one uppercase letter and one number' : '';
+  }
+
+  getCheckPasswordErrorMessage() {
+    return this.registerForm.get('confirmPassword').hasError('required') ? 'You must enter a value' :
+    this.registerForm.hasError('notSame') ? 'Passwords must be the same' : '';
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      console.log('Valid');
+    } else {
+      console.log('Invalid');
+    }
   }
 }
